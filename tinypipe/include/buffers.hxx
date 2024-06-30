24dd3ccfc34931a81ca646962751c3ed4330c1fc
EN <109923311+CardboardDog@users.noreply.github.com>
2024-06-30 15:23:08 -0400

    Vertex Attribute Arrays
    - added vertex attribute arrays
    - added glad

---
+++ tinypipe/include/buffers.hxx
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<glad/glad.h>
+namespace tinypipe{
+    class VertexAttributeArray{
+        public:
+        VertexAttributeArray(int length);
+        ~VertexAttributeArray();
+        void use();
+        void addAttribute(GLuint index, GLint length, GLenum type, bool normalized);
+        void disableAttribute(GLuint index);
+        void enableAttribute(GLuint index);
+        protected:
+        GLintptr addPointer = 0;
+        unsigned int VAO;
+        int attributeCount = 0;
+    };
+};
---
+++ tinypipe/source/buffers.cxx
@@ -0,0 +1,41 @@
+#include"buffers.hxx"
+using namespace tinypipe;
+VertexAttributeArray::VertexAttributeArray(int length){
+    glGenVertexArrays(1,&this->VAO);
+    this->addPointer = 0;
+    this->attributeCount = length;
+}
+VertexAttributeArray::~VertexAttributeArray(){
+    glDeleteVertexArrays(1,&this->VAO);
+}
+void VertexAttributeArray::use(){
+    glBindVertexArray(this->VAO);
+}
+void VertexAttributeArray::addAttribute(GLuint index, GLint length, GLenum type, bool normalized){
+    GLsizei size;
+    switch(type){
+        case GL_FLOAT:
+            size = sizeof(float)*length;
+            break;
+        case GL_UNSIGNED_INT:
+        case GL_INT:
+            size = sizeof(int)*length;
+            break;
+        case GL_DOUBLE:
+            size = sizeof(double)*length;
+        default:
+            return;
+    };
+    glBindVertexArray(this->VAO);
+    glVertexAttribPointer(index,length,type,(normalized)?GL_TRUE:GL_FALSE,sizeof(float)*this->attributeCount,(void*)this->addPointer);
+    glEnableVertexAttribArray(index);
+    this->addPointer+=size;
+}
+void VertexAttributeArray::enableAttribute(GLuint index){
+    glBindVertexArray(this->VAO);
+    glEnableVertexAttribArray(index);   
+}
+void VertexAttributeArray::disableAttribute(GLuint index){
+    glBindVertexArray(this->VAO);
+    glDisableVertexAttribArray(index);   
+}
